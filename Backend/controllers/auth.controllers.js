670ad35e3317d07b2935995efad7b05ab2595b08
BilalTeli <telibilal959@gmail.com>
2026-01-11 20:43:48 +0530

    token generated

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import genToken from "../utils/token.js"
 
-const signUp=async(req,res)=>{
+export const signUp=async(req,res)=>{
     try{
         const {fullName,email,password,mobile,role}=req.body
         const user=await User.findOne({email})
@@ -24,9 +25,50 @@ const signUp=async(req,res)=>{
             mobile,
             password:hashedPassword
         })
+
+        const token=await genToken(user._id)
+        res.cookie("token",token,{
+            secure:false,
+            sameSite:"strict",
+            maxAge:7*24*60*60*1000,
+            httpOnly:true
+        })
+        return res.status(201).json(user)
     }
     catch(error)
     {
-        console.log("error")   
+       return res.status(500).json(`sign up error ${error}`)  
+    }
+}
+export const signIn=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"user doesn't exists"})
+        }
+        
+
+        const hashedPassword=await bcrypt.hash(password,10)
+        user=await User.create({
+            fullName,
+            email,
+            role,
+            mobile,
+            password:hashedPassword
+        })
+
+        const token=await genToken(user._id)
+        res.cookie("token",token,{
+            secure:false,
+            sameSite:"strict",
+            maxAge:7*24*60*60*1000,
+            httpOnly:true
+        })
+        return res.status(201).json(user)
+    }
+    catch(error)
+    {
+       return res.status(500).json(`sign up error ${error}`)  
     }
 }
